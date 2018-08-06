aca7b79dbb366efa770416be33a197c38877f1e7
pravinyaduvanshi <praviny156@gmail.com>
2018-08-06 19:44:38 +0530

    Program to Compute Quotient and Remainder

---
+++ C/day-2/divide.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main(){
+
+    int dividend, divisor, quotient, remainder;
+
+    printf("Enter dividend: ");
+    scanf("%d", &dividend);
+
+    printf("Enter divisor: ");
+    scanf("%d", &divisor);
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+
+    printf("Quotient = %d\n", quotient);
+    printf("Remainder = %d", remainder);
+
+    getch();
+}
