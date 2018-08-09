a40734c7af56cc0bdc5b21a6233559163d8b9969
pravinyaduvanshi <praviny156@gmail.com>
2018-08-09 07:11:42 +0530

    program for find average of 3 numbers

---
+++ C/day-5/average.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+  int a,b,c, sum;
+  float d;
+
+  printf("Please enter 3 numbers");
+  scanf("%d%d%d",&a,&b,&c);
+  
+  sum=a+b+c;
+  d=(a+b+c)/3;
+  
+  printf("\nSum is %d", sum);
+  printf("\nAverage is  %f",d);
+  getch();
+
+}
