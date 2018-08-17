9f9f9c06ee37bf886e7fdd7816f944b2da3dd622
pravinyaduvanshi <praviny156@gmail.com>
2018-08-17 22:11:59 +0530

    program for check character is vowel or not

---
+++ C/day-13/vowel.c
@@ -0,0 +1,29 @@
+//program for check character is vowel or not
+#include<stdio.h>
+
+int main()
+{
+    char ch;
+    printf("Input a Character =  ");
+    scanf("%c", &ch);
+
+    switch(ch)
+    {
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+            printf("\n %c is a vowel.\n\n", ch);
+            break;
+        default:
+            printf("%c is not a vowel.\n\n", ch);
+    }
+
+    return 0;
+}
