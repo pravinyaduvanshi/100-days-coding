4037ab706ee63fd6a1107a41c49d46e75ac051f1
pravinyaduvanshi <praviny156@gmail.com>
2018-08-19 21:40:31 +0530

    C program to convert uppercase string to lower case

---
+++ C/day-15/lower.c
@@ -0,0 +1,18 @@
+// C program to convert uppercase string to lower case
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+   
+   char str[25];
+   int i;
+   printf("Enter the string: ");
+   scanf("%s",str);
+ 
+   for(i=0;i<=strlen(str);i++){
+      if(str[i]>=65&&str[i]<=90)
+         str[i]=str[i]+32;
+   }
+   printf("\nLower Case String is: %s",str);
+   return 0;
+}
