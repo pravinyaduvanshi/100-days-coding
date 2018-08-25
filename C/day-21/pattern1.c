03c683c117080c4097455f169a2c991c09734f22
pravinyaduvanshi <praviny156@gmail.com>
2018-08-25 22:42:39 +0530

    star pattern

---
+++ C/day-21/pattern1.c
@@ -0,0 +1,31 @@
+/*
+_____________________________
+    *
+   **
+  ***
+ ****
+*****
+____________________________
+*/
+
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    printf("how many number of raw do you want for print *"\n);
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j>=n+1-i){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+
+    }
+}
