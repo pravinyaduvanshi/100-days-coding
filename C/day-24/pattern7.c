c89d5d95b80c61a06b3949b0f68220e4f0a0943d
pravinyaduvanshi <praviny156@gmail.com>
2018-08-28 21:26:25 +0530

    star pattern

---
+++ C/day-24/pattern7.c
@@ -0,0 +1,28 @@
+/*
+   1
+  121
+ 12321
+1234321
+*/
+#include<stdio.h>
+void main()
+{
+    int i,j,k;
+    for(i=1;i<=4;i++)
+    {
+        k=1;
+        for(j=1;j<=7;j++)
+        {
+            if(j>=5-i && j<=3+i){
+                printf("%d",k);
+                j<4? k++ : k--;
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+
+    }
+}
+
