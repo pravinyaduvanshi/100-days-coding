89b505e40e02ff7dc6fef5555de173b736539039
pravinyaduvanshi <praviny156@gmail.com>
2018-08-29 21:04:14 +0530

    STAR PATTERN

---
+++ C/day-25/pattern8.c
@@ -0,0 +1,31 @@
+/*
+ABCDCBA
+ABC CBA
+AB   BA
+A     A
+*/
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    char k;
+    for(i=1;i<=4;i++)
+    {
+        k='A';
+        for(j=1;j<=7;j++)
+        {
+            if(j<=5-i || j>=3+i){
+                printf("%c",k);
+                j<4? k++ : k--;
+            }
+            else{
+                printf(" ");
+                if(j==4)
+                    k--;
+            }
+        }
+        printf("\n");
+
+    }
+}
+
