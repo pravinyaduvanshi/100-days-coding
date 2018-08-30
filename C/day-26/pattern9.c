4ca4d9fbefdbc83f38a8d310bad0781bdc92ff93
pravinyaduvanshi <praviny156@gmail.com>
2018-08-30 22:11:09 +0530

    star pattern

---
+++ C/day-26/pattern9.c
@@ -0,0 +1,54 @@
+/*
+for odd number of rows (7)
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+ *
+
+ for even number of rows (8)
+   *
+  ***
+ *****
+*******
+*******
+ *****
+  ***
+   *
+
+*/
+#include<stdio.h>
+void main()
+{
+    int i,j,k=0,rows,n;
+    printf("enter number of raws");
+    scanf("%d",&rows);
+    n=(rows+1)/2;
+
+    for(i=1;i<=rows;i++)
+    {
+        if(rows%2==0)
+        {
+            if(i<=n)
+                k++;
+            if(i>n+1)
+                k--;
+        }
+        else
+        i<=n?k++:k--;
+
+        for(j=1;j<=rows;j++)
+        {
+            if(j>=n+1-k && j<=n-1+k)
+                printf("*");
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+
+    }
+}
+
