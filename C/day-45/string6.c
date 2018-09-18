fee59dfe7c8d8e49fe790854dea96ed701d10a84
pravinyaduvanshi <praviny156@gmail.com>
2018-09-18 21:18:28 +0530

    string program

---
+++ C/day-45/string6.c
@@ -0,0 +1,17 @@
+// Program to find the length of a string
+
+#include <stdio.h>
+int main()
+{
+    char s[1000], i;
+
+    printf("Enter a string: ");
+
+    scanf("%[^\n]s", s);
+
+    for(i = 0; s[i] != '\0'; ++i);
+
+    printf("Length of string: %d", i);
+
+    return 0;
+}
